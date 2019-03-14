db888c85402758e664e673bb6f1ebecd988bc12c
L-Chris <563303226@qq.com>
2019-03-15 00:04:13 +0800

    feat: 完善界面

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <VApp class="app">
+    <VContent>
+      <VContainer fluid>
+        <VAutocomplete
+          v-model="hostName"
+          :items="hostNames"
+          label="请输入环境名称"
+        /></VAutocomplete>
+        <VList>
+          <template v-for="_ in filteredHosts">
+            <VListTile :key="_.name" @click="handleClick(_)">
+              <VListTileContent v-html="_.name"/>
+            </VListTile>
+            <VDivider :key="_.id"/>
+          </template>
+        </VList>
+      </VContainer>
+    </VContent>
+  </VApp>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      hostName: '',
+      hosts: [
+        { id: 0, name: 'student-apphd-exam' },
+        { id: 1, name: 'parent-apphd-exam' }
+      ]
+    }
+  },
+  computed: {
+    hostNames() {
+      return this.hosts.map(_ => _.name)
+    },
+    filteredHosts() {
+      return this.hosts.filter(_ => _.name.includes(this.hostName))
+    }
+  },
+  methods: {
+    handleClick(item) {
+      console.log(item)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.app{
+}
+</style>
